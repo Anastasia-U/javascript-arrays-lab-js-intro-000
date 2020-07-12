d3eb39187bc78eb0b7df64572fce5bd29d5dca81
anastasia Ukhtinskaya <anastassia94@gmail.com>
2020-07-12 21:27:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,18 @@
     kittens.push(name);
     return kittens;
   }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
